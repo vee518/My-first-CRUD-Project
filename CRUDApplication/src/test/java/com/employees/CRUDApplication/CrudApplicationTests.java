17db5d6e4fa6bc3d8c3e17f674a4c1de1c4a68d1
vee518 <you@example.com>
2024-07-26 14:58:57 +0000

    first commit

---
+++ CRUDApplication/src/test/java/com/employees/CRUDApplication/CrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.employees.CRUDApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
